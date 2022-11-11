2092314248ae5422b768a84a5aac81de7481c3b2
Walace da Silva <walacespo@gmail.com>
2022-11-11 11:26:40 -0300

    Inclusion of routes permission

--- backend/src/main/java/com/devsuperior/dsmovie/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/config/ResourceServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -23,6 +24,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
+	private static final String[] ALL_USER_GET = { "/movies/**" };
+	
+	private static final String[] ALL_USER_PUT = { "/scores/**" };
+	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
@@ -38,6 +43,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-		.anyRequest().authenticated();
+		.antMatchers(HttpMethod.GET, ALL_USER_GET).permitAll()
+		.antMatchers(HttpMethod.PUT, ALL_USER_PUT).permitAll()
+		.anyRequest().hasAnyRole("ADMIN");
 	}	
 }
